f594c44b530d93021eeaabcbca27b07270fdafd3
Joshua <73680704+BarcodeBimbo@users.noreply.github.com>
2025-01-04 12:47:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/create-pdf', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5566;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
